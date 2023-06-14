9d37ebc136f6ca1c056182a9d4616a79f46af98a
XuF163 <121853480+XuF163@users.noreply.github.com>
2023-06-14 20:44:48 +0800

    Update pymon.sh

--- pymon.sh
+++ pymon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo"如果遇到代码不跑的情况就按回车 yes or no 的选项直接y  2023-6更新"
-sudo -i  # 获取root权限，如果本来就以root权限登录，那可以跳过
+sudo su root # 获取root权限，如果本来就以root权限登录，那可以跳过
 
 # 更新软件包并升级系统
 apt update -y && apt upgrade -y
