1bea884647b46090e17b3147c0c4c90380993954
XuF163 <121853480+XuF163@users.noreply.github.com>
2023-06-14 19:19:50 +0800

    Update pymon.sh

--- pymon.sh
+++ pymon.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-apt update && apt upgrade  # 更新系统
-apt install pipx  # 安装pipx
-pipx ensurepath  # 把pipx添加到环境变量
+apt update && apt upgrade&&apt install pipx&&pipx ensurepath 
 gnome-terminal -e --window
 pipx install nb-cli  # 安装nonebot脚手架
 nb self install nb-cli-plugin-littlepaimon  # 安装小派蒙脚手架插件
