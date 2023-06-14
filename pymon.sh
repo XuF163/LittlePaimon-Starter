fb0a32a5b4488feeb3624b9a2f06f0d5e812cc04
XuF163 <121853480+XuF163@users.noreply.github.com>
2023-06-14 19:25:14 +0800

    Update pymon.sh

--- pymon.sh
+++ pymon.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
-apt update && apt upgrade  # 更新系统
-apt install pipx  # 安装pipx
-pipx ensurepath  # 把pipx添加到环境变量
-pipx install nb-cli  # 安装nonebot脚手架
-nb self install nb-cli-plugin-littlepaimon  # 安装小派蒙脚手架插件
-nb paimon create  # 创建小派蒙项目
-cd LittlePaimon  # LittlePaimon为你刚刚填写的项目目录名
-nb paimon res  # 给出的两次选择均保持默认(回车两次)
-nb paimon run playwright install chromium --with-deps  # 安装playwright相关依赖
-echo "到此，您的小派蒙Bot安装成功！
 
-此后，你只需要在派蒙的目录下使用命令nb paimon run即可启动小派蒙Bot，并根据你选择的go-cqhttp方式，启动并登录机器人账号。"
+# 更新软件包并升级系统
+apt update && apt upgrade -y
+
+# 安装 pipx
+apt install -y python3-pip
+pip install --user pipx
+export PATH="$HOME/.local/bin:$PATH"
+
+# 设置 pipx 环境变量
+pipx ensurepath
+
+# 安装 nb-cli
+pipx install nb-cli
+
+# 安装 nonebot 脚手架
+nb self install nb-cli-plugin-littlepaimon
+
+# 创建项目并设置相关配置
+nb paimon create
+cd LittlePaimon
+nb paimon res
+
+# 安装 Playwright 依赖
+nb paimon run playwright install chromium --with-deps
+
+# 等待一段时间
+sleep 114514
+
+echo "脚本执行完毕"
+
