c56afb37b1c7c607d1a959fd9284d7f7cdd5a680
XuF163 <121853480+XuF163@users.noreply.github.com>
2023-06-14 22:15:09 +0800

    Update pymon.sh

--- pymon.sh
+++ pymon.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
+
 echo "如果遇到代码不跑的情况就按回车 yes or no 的选项直接y  2023-6更新"
-sudo su root # 获取root权限，如果本来就以root权限登录，那可以跳过
+
+# 获取 root 权限
+if [[ $EUID -ne 0 ]]; then
+    echo "需要以 root 权限运行脚本，请输入 root 密码："
+    sudo -v
+    if [[ $? -ne 0 ]]; then
+        echo "认证失败，请检查密码是否正确。"
+        exit 1
+    fi
+fi
 
 # 更新软件包并升级系统
-apt update -y && apt upgrade -y
+sudo apt update -y && sudo apt upgrade -y
 
 # 安装 pipx
-apt install -y python3-pip
+sudo apt install -y python3-pip
 pip install --user pipx
-export PATH="$HOME/.local/bin:$PATH"
 
 # 设置 pipx 环境变量
 pipx ensurepath
+source ~/.bashrc
+source ~/.profile
 
 # 安装 nb-cli
 pipx install nb-cli
-import os
-a = os.system('clear')
-reset
+
 # 安装 nonebot 脚手架
 nb self install nb-cli-plugin-littlepaimon
-#帮你擦下屏幕
+
+# 清屏
 clear
 
-#提示
-echo "准备敲回车键力"
-# 创建项目并设置相关配置
+# 提示
+echo "准备敲回车键继续"
+read -r
 
-nb paimon create&&cd LittlePaimon&&nb paimon res&&nb paimon run playwright install chromium --with-deps
+# 创建项目并设置相关配置
+nb paimon create && cd LittlePaimon && nb paimon res && nb paimon run playwright install chromium --with-deps
+
