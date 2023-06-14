dbd12a410459755e71d9cea72767058198c53db4
XuF163 <121853480+XuF163@users.noreply.github.com>
2023-06-14 21:47:52 +0800

    Update pymon.sh

--- pymon.sh
+++ pymon.sh
@@ -15,9 +15,7 @@ pipx ensurepath
 
 # 安装 nb-cli
 pipx install nb-cli
-import os
-a = os.system('clear')
-sudo su root
+
 source ~/.bashrc
 # 安装 nonebot 脚手架
 nb self install nb-cli-plugin-littlepaimon
